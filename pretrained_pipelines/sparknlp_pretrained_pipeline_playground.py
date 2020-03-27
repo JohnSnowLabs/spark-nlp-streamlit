66df10a8af801142b7aa63d025b05ca516018311
Veysel Kocaman <vkocaman@gmail.com>
2020-03-27 14:20:39 +0100

    updated with new modes and 2.4.4

--- pretrained_pipelines/sparknlp_pretrained_pipeline_playground.py
+++ pretrained_pipelines/sparknlp_pretrained_pipeline_playground.py
@@ -9,9 +9,34 @@ from sparknlp.pretrained import PretrainedPipeline
 import pandas as pd
 
 import sparknlp
-spark = sparknlp.start()
 
-import streamlit as st
+#spark = sparknlp.start()
+
+
+#%%writefile streamlit_healthcare.py
+from pyspark.sql import SparkSession
+
+import os
+import sys
+
+import streamlit as st 
+
+import os
+
+import pandas as pd
+
+jar_path = "../jars/"
+
+spark = SparkSession.builder \
+        .appName("Spark NLP Enterprise 2.4.2") \
+        .master("local[8]") \
+        .config("spark.driver.memory","4G") \
+        .config("spark.driver.maxResultSize", "1G") \
+        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+        .config("spark.kryoserializer.buffer.max", "1000M")\
+        .config("spark.jars", "{}spark-nlp-assembly-2.4.3.jar".format(jar_path)) \
+        .getOrCreate()
+
 
 SPARK_NLP_PIPELINES = ['explain_document_ml',
 'explain_document_dl',
@@ -34,14 +59,20 @@ DEFAULT_TEXT = "Other than being the king of the north, John Snow is a an englis
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 
+
 @st.cache(allow_output_mutation=True)
 def load_pipeline(name):
+    #if name=='match_datetime':
+        #return light_Datetime
+    #else:
     return PretrainedPipeline(name, lang='en')
 
 
 @st.cache(allow_output_mutation=True)
 def process_text(model_name, text, mode='slim'):
+    
     pipeline = load_pipeline(model_name)
+
     if mode=='slim':
         return pipeline.annotate(text)
     else:
@@ -66,7 +97,7 @@ text = st.text_area("Text to analyze", DEFAULT_TEXT)
 try:
     annotated_text = process_text(sparknlp_model, text, mode='slim')
     full_annotated_text = process_text(sparknlp_model, text, mode='full')[0]
-except:
+except Exception as e:
     st.write('error in loading the pipeline !')
     annotated_text={}
     full_annotated_text={}
@@ -84,8 +115,8 @@ def get_color():
     r = lambda: random.randint(100,255)
     return '#%02X%02X%02X' % (r(),r(),r())
 
-
-def get_onto_NER_html (annotated_text):
+    
+def get_onto_NER_html (annotated_text, labels):
     
     light_data=annotated_text
     
@@ -104,47 +135,55 @@ def get_onto_NER_html (annotated_text):
     for l in label_list:
         
         label_color[l]=get_color()
-        
-    
+            
     for index, this_token in enumerate(light_data['token']):
 
-        if light_data['ner'][index]=='O':
-            html_output+=this_token + " "
-        else:
-            color = label_color[light_data['ner'][index].split('-')[1]]
+        try:
+            ent = light_data['ner'][index].split('-')[1]
+        except:
+            ent = light_data['ner'][index]
+        
+       
+        if ent in labels:
+            color = label_color[ent]
             html_output+='<SPAN style="background-color: {}">'.format(color) + this_token + " </SPAN>"
+        else:
+            html_output+=this_token + " "
         
 
     html_output += '</div>'
     html_output += '<div>Color codes:'
 
-    for l in label_list:
+    for l in labels:
         
         html_output += '<SPAN style="background-color: {}">{}</SPAN>, '.format(label_color[l],l)
    
     return html_output
     
-    
-if 'ner' in annotated_text.keys():
-    
-    st.header("Named Entities")
+
+
+def show_html(annotated_text):
+
+    st.header("Named Entities ({})".format(sparknlp_model))
     st.sidebar.header("Named Entities")
-    
+
+    #st.write(annotated_text['ner'])
     label_set = list(set([i.split('-')[1] for i in annotated_text['ner'] if i!='O']))
-    
-    #['LOC','PER','ORG','MISC']
-    
+
     labels = st.sidebar.multiselect(
-        "Entity labels", options=label_set, default=list(label_set)
-    )
-    html = get_onto_NER_html (annotated_text) #displacy.render(doc, style="ent", options={"ents": labels})
-    # Newlines seem to mess with the rendering
+            "Entity labels", options=label_set, default=list(label_set)
+        )
+        
+    html = get_onto_NER_html (annotated_text, labels) 
+        # Newlines seem to mess with the rendering
     html = html.replace("\n", " ")
     st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
-    
+
     st.write('')
     st.write('')
 
+
+    
 if sparknlp_model == 'explain_document_dl':
     
     df = pd.DataFrame({'token':annotated_text['token'], 'label':annotated_text['ner'],
@@ -154,6 +193,8 @@ if sparknlp_model == 'explain_document_dl':
     
 
 elif sparknlp_model == 'explain_document_ml':
+    
+    #st.write(annotated_text)
     
     df = pd.DataFrame({'token':annotated_text['token'], 
                       'corrected':annotated_text['spell'], 'POS':annotated_text['pos'],
@@ -200,18 +241,27 @@ elif sparknlp_model == 'clean_slang':
     
 if 'entities' in annotated_text.keys():
     st.write('')
-    st.write('Entities')
+    st.write('Named Entities')
     st.write('')
     
     chunks=[]
     entities=[]
+
+    #html = get_onto_NER_html (annotated_text)
+    #html = html.replace("\n", " ")
+    #st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
+    show_html(annotated_text)
+
     for n in full_annotated_text['entities']:
         
         chunks.append(n.result)
         entities.append(n.metadata['entity']) 
-        
+    
+    st.write('')
+    st.write('Entities')
     st.dataframe(pd.DataFrame({'chunks':chunks, 'entities':entities}))
     #st.write(annotated_text['entities'])
+    
     
     
 if 'sentence' in annotated_text.keys():
@@ -228,3 +278,9 @@ if 'sentiment' in annotated_text.keys():
     st.write('')
     st.dataframe(pd.DataFrame({'sentence':annotated_text['sentence'], 'sentiment':annotated_text['sentiment']}))
     
+    
+st.subheader('Model Output')
+st.write(annotated_text)
+
+st.sidebar.markdown("Spark NLP version: {}".format(sparknlp.version()))
+st.sidebar.markdown("Apache Spark version: {}".format(spark.version))
